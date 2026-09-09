b40543fbc814df01768d705c91255c2399711877
MoritzWM <github@moritzwm.de>
2026-09-09 13:00:02 +0200

    fix regex pattern

--- src/pytom_tm/extract.py
+++ src/pytom_tm/extract.py
@@ -332,7 +332,7 @@ def extract_particles(
 
     # remove relion5 reconstructed tomogram name as it messes with linking the tilt
     # series id when extracting subtomos
-    warp_match = re.match(r"(.*)(_\d+\.\d+Apx\.mrc)", tomogram_id)
+    warp_match = re.match(r"(.*)(_\d+\.\d+Apx)", tomogram_id)
     if relion5_compat and tomogram_id.startswith("rec_"):
         tomogram_id = tomogram_id[4:]
     # replace Warp suffix with .tomostar for compatibility with WarpTools
